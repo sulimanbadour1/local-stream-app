b77708f38f5f3267f01572dae3a9f1ae793f19d4
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-12-04 02:04:58 +0100

    Update movie page layout and background image

--- local-movie-streamer-frontend/src/pages/Shows/movie/index.jsx
+++ local-movie-streamer-frontend/src/pages/Shows/movie/index.jsx
@@ -76,9 +76,18 @@ const Movie = () => {
   // let bg = `https://image.tmdb.org/t/p/original/${data.backdrop_path}`;
   return (
     <div className=" items-center justify-center content-center">
-      <div className="px-4 md:px-48 mt-12 flex mx-auto justify-center mb-12">
+      <div
+        className="px-12 md:px-48 mt-12 flex mx-auto justify-center mb-12
+      bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${singleMovie.backdrop_path})`,
+        }}
+      >
         <div
-          className={`p-8 flex gap-10 border border-gray-600/10 rounded-lg flex-col md:flex-col lg:flex-row justify-center content-center items-center `}
+          className={`p-8 flex gap-10 border border-gray-600/10 rounded-lg
+          backdrop-blur-xl bg-white/30
+           flex-col md:flex-col lg:flex-row justify-center content-center items-center
+           m-12 `}
         >
           <div className="relative z-10">
             <img
